02fb82a7ad556178911b38d0319bc81c05a10d34
Shafik93 <40148585+Shafik93@users.noreply.github.com>
2018-07-20 20:20:03 -0400

    update

--- CSC4710/src/user/dao/UserDao.java
+++ CSC4710/src/user/dao/UserDao.java
@@ -46,7 +46,7 @@ public class UserDao {
 	final public String conInfo = "jdbc:mysql://127.0.0.1:3306/sampledb?"
             + "user=root&password=Shafiko93!";
 	
-	final public String connector = "com.mysql.cj.jdbc.Driver";
+	final public String connector = "com.mysql.jdbc.Driver";
 	
 	public Manager findByUsername1(String musername) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		Manager manager = new Manager();
