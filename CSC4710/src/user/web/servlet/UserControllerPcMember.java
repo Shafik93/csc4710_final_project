e09747ff6b35d7387afb45a99f208090f20b56d5
Adeeba Zaidi <a.zaidi1215@gmail.com>
2018-07-22 19:09:42 -0400

    Partial Update and Delete for PCMember

--- CSC4710/src/user/web/servlet/UserControllerPcMember.java
+++ CSC4710/src/user/web/servlet/UserControllerPcMember.java
@@ -1,6 +1,11 @@
 package user.web.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import user.dao.UserDao;
+import user.domain.PcMember;
 
 /**
  * Servlet implementation class UserControllerPcMember
@@ -17,6 +23,8 @@ import user.dao.UserDao;
 @WebServlet("/UserControllerPcMember")
 public class UserControllerPcMember extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static String pcmember = "PcMemberServlet";
+	private static String pcmemberform = "/Queryresult/pcmemberform.jsp";
 	UserDao dao = new UserDao();
     /**
      * @see HttpServlet#HttpServlet()
@@ -36,20 +44,56 @@ public class UserControllerPcMember extends HttpServlet {
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	// TODO Auto-generated method stub
 	String action  = request.getParameter("action");
+	String forward = "";
 	
 	if(action.equalsIgnoreCase("delete")){
 		int memberid = Integer.parseInt(request.getParameter("memberid"));
 		try {
 			dao.deletePcmember(memberid);
-			
+			forward = pcmember;
 		} catch (InstantiationException | IllegalAccessException
 				| ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	RequestDispatcher view = request.getRequestDispatcher("PcMemberServlet");
+	else if (action.equalsIgnoreCase("edit")){
+		int memberid = Integer.parseInt(request.getParameter("memberid"));
+		try {
+			PcMember pcmember = findallByid(memberid);
+			request.setAttribute("pcmember", pcmember);
+			forward = pcmemberform;
+		} catch (InstantiationException | IllegalAccessException
+				| ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	RequestDispatcher view = request.getRequestDispatcher(forward);
 	view.forward(request, response);
 }
+
+public PcMember findallByid(int memberid) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+	PcMember pcmember = new PcMember();
+    try {
+    	Class.forName(dao.connector).newInstance();
+		Connection connect = DriverManager.getConnection(dao.conInfo);
+        PreparedStatement preparedStatement = connect.prepareStatement("select * from pcmember where memberid=?");
+        preparedStatement.setInt(1, memberid);
+        ResultSet rs = preparedStatement.executeQuery();
+
+        if (rs.next()) {
+            pcmember.setMemberid(rs.getInt("memberid"));
+            pcmember.setName(rs.getString("name"));
+            pcmember.setEmail(rs.getString("email"));
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    return pcmember;
+}
+
 
 }
