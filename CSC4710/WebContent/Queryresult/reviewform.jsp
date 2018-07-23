5437b53fcfec4265728e45fbc4e15e774cc976bd
Shafik93 <40148585+Shafik93@users.noreply.github.com>
2018-07-22 20:13:10 -0400

    function implementation
    delete and update for review

---
+++ CSC4710/WebContent/Queryresult/reviewform.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Update review</title>
+</head>
+<body>
+
+<form action="UserControllerReview" method ="post">
+
+    <input type="hidden" name="method" value="login"/>
+		Date :	<input type="text" name="date" value="${review.sdate}" /><br/>
+		Comment :	<input type="text" name="comment"  value="${review.comment}" /><br/>
+		Recommendation :	<input type="text" name="recommendation"  value="${review.recommendation}" /><br/>
+		PaperId :	<input type="text" name="paperid" value="${review.paperid}" /><br/>
+		Email :	<input type="text" name="email" value="${review.email}" /><br/>
+	<input type="submit" value="Submit" name = "frmsub" />
+</form>
+</body>
+</html>
--- CSC4710/src/user/web/servlet/UserControllerReview.java
+++ CSC4710/src/user/web/servlet/UserControllerReview.java
@@ -1,6 +1,11 @@
 package user.web.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import user.dao.UserDao;
+import user.domain.Paper;
+import user.domain.Review;
 import user.service.PaperService;
 
 
@@ -19,7 +26,10 @@ import user.service.PaperService;
 @WebServlet("/UserControllerReview")
 public class UserControllerReview extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static String reviewserv = "ReviewServlet";
+	private static String reviewform = "/Queryresult/reviewform.jsp";
     UserDao dao = new UserDao();
+    private static int p = 0;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,7 +44,35 @@ public class UserControllerReview extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doPost(request, response);
+		String action  = request.getParameter("action");
+		String forward = "";
+		if(action.equalsIgnoreCase("delete")){
+			int reportid = Integer.parseInt(request.getParameter("reportid"));
+			try {
+				dao.deleteReview(reportid);
+				forward = reviewserv;
+				
+			} catch (InstantiationException | IllegalAccessException
+					| ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		else if(action.equalsIgnoreCase("edit")){
+			int reportid = Integer.parseInt(request.getParameter("reportid"));
+			try{
+				Review review = findallByid(reportid);
+				request.setAttribute("review", review);
+				forward = reviewform;
+			} catch (InstantiationException | IllegalAccessException
+					| ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		RequestDispatcher view = request.getRequestDispatcher(forward);
+		view.forward(request, response);
 		
 	}
 
@@ -45,21 +83,52 @@ public class UserControllerReview extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String action  = request.getParameter("action");
-		
-		if(action.equalsIgnoreCase("delete")){
-			int reportid = Integer.parseInt(request.getParameter("reportid"));
+		if(request.getParameter("frmsub") != null){
+			Review review = new Review();
+			review.setReportid(p);
+            review.setSdate(request.getParameter("sdate"));
+            review.setComment(request.getParameter("comment"));
+            review.setRecommendation(request.getParameter("recommendation"));
+            review.setPaperid(Integer.parseInt(request.getParameter("paperid")));
+            review.setEmail(request.getParameter("email"));
+			
 			try {
-				dao.deleteReview(reportid);
-				
+				dao.updateReview(review);
 			} catch (InstantiationException | IllegalAccessException
 					| ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}
-		RequestDispatcher view = request.getRequestDispatcher("ReviewServlet");
-		view.forward(request, response);
+			RequestDispatcher view = request.getRequestDispatcher(reviewserv);
+			view.forward(request, response);
+			}
+		
 	}
+public Review findallByid(int reportid) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		Review review = new Review();
+        try {
+        	Class.forName(dao.connector).newInstance();
+			Connection connect = DriverManager.getConnection(dao.conInfo);
+            PreparedStatement preparedStatement = connect.
+                    prepareStatement("select * from review where reportid=?");
+            preparedStatement.setInt(1, reportid);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+                review.setReportid(rs.getInt("reportid"));
+                review.setSdate(rs.getString("sdate"));
+                review.setComment(rs.getString("comment"));
+                review.setRecommendation(rs.getString("recommendation"));
+                review.setPaperid(rs.getInt("paperid"));
+                review.setEmail(rs.getString("email"));
+                p = rs.getInt("reportid");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return review;
+    }
 
 }
